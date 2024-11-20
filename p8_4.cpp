ad7379d6db33338178b96b287ffacbd08320bce3
emawnag <wanghongni02@gmail.com>
2024-11-21 04:23:47 +0800

    Update p8_4.cpp

--- p8_4.cpp
+++ p8_4.cpp
@@ -1,112 +1,167 @@
 #include "p8_4.h"
+#include <fstream>
 #include <vector>
 #include <string>
 #include <tuple>
+#include <sstream>
 #include <map>
 #include <algorithm>
 #include <functional>
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
 
 using namespace std;
 
-struct edgeD
-{
-    string fromV;
-    string toV;
-    int weight;
-};
+#define WLOG(X)                                                          \
+    string tmp = "wget -q --spider http://127.0.0.1:55125/dot?f=" + (X); \
+    system(tmp.c_str());
+#define NL "@NL@"
+#define AR "@AR@"
+#define AL "@AL@"
 
-struct node
-{
-    bool isLeaf;
-    int height;
-};
+map<string, int> nodeHeight;
 
-long long Minimum_Build_Time(vector<
-                             tuple<string, vector<string>, int>> &makefile)
-{
-
-    vector<edgeD> graph;
-    map<string, node> vertex;
-    map<string, int> mf;
-    vector<edgeD> tmp_h_eq_1;
-
-    for (auto &i : makefile)
+string nodeHtml(){
+    string ans = "";
+    for (const auto &obj : nodeHeight)
     {
-        mf[get<0>(i)] = get<2>(i);
-        for (auto &j : get<1>(i))
-        {
-            edgeD temp;
-            temp.toV = get<0>(i);
-            temp.fromV = j;
-            temp.weight = get<2>(i);
-            graph.push_back(temp);
+        ans += "\"" + obj.first + "\" [label=" + AL + AL + "TABLE BORDER=\"0\" CELLBORDER=\"1\" CELLSPACING=\"0\"" + AR + AL + "TR" + AR + AL + "TD" + AR + obj.first + AL + "/TD" + AR + AL + "/TR" + AR + AL + "TR" + AR + AL + "TD" + AR + to_string(obj.second) + AL + "/TD" + AR + AL + "/TR" + AR + AL + "/TABLE" + AR+ AR + "];" + NL;
+    }
+    return ans;
+}
 
-            if (j.size() >= 4 && j.substr(j.size() - 4) == ".cpp")
+string Generate_Graphviz(const map<string, tuple<vector<string>, int>> &makefile)
+{
+    ostringstream outfile;
+    outfile << "digraph G {" << NL;
+
+    for (const auto &obj : makefile)
+    {
+        string target = obj.first;
+        const vector<string> &dependencies = get<0>(obj.second);
+
+        for (const auto &dep : dependencies)
+        {
+            outfile << "    \"" << dep << "\" -" << AR << " \"" << target << "\"" << "[label=\"" << to_string(get<1>(obj.second)) << "\"]" << ";" << NL;
+        }
+    }
+
+    outfile <<nodeHtml()<< "}" << NL;
+
+    return outfile.str();
+}
+
+static const std::string base64_chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789+-";
+
+std::string base64_encode(const std::string &input)
+{
+    std::string encoded;
+    unsigned char const *bytes_to_encode = reinterpret_cast<const unsigned char *>(input.c_str());
+    size_t in_len = input.length();
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+
+    while (in_len--)
+    {
+        char_array_3[i++] = *(bytes_to_encode++);
+        if (i == 3)
+        {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+
+            for (i = 0; i < 4; i++)
             {
-                vertex[j] = {true, -1};
+                encoded += base64_chars[char_array_4[i]];
             }
-            else if (j.size() >= 4 && j.substr(j.size() - 4) == ".exe")
+            i = 0;
+        }
+    }
+
+    if (i)
+    {
+        for (j = i; j < 3; j++)
+        {
+            char_array_3[j] = '\0';
+        }
+
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+
+        for (j = 0; (j < i + 1); j++)
+        {
+            encoded += base64_chars[char_array_4[j]];
+        }
+
+        while ((i++ < 3))
+        {
+            encoded += '=';
+        }
+    }
+
+    return encoded;
+}
+
+bool ends_with_cpp(const std::string &str)
+{
+    return str.size() >= 4 && str.compare(str.size() - 4, 4, ".cpp") == 0;
+}
+
+
+void computeHeight(const map<string, tuple<vector<string>, int>> &mf, string name, int ih)
+{
+    if (nodeHeight.find(name) != nodeHeight.end())
+    {
+       nodeHeight[name] = nodeHeight[name]>ih?nodeHeight[name]:ih;
+    }
+    else
+    {
+        nodeHeight[name] = ih;
+    }
+
+    if(ends_with_cpp(name)){
+        return;
+    }else{
+        for (const auto &dep : get<0>(mf.at(name)))
+        {
+            computeHeight(mf, dep, ih +1);
+        }
+    }
+}
+
+long long Minimum_Build_Time(vector<tuple<string, vector<string>, int>> &makefile)
+{
+    nodeHeight["root"] = 0;
+    map<string, tuple<vector<string>, int>> makefile_copy;
+    for (const auto &obj : makefile)
+    {
+        makefile_copy[get<0>(obj)] = {get<1>(obj), get<2>(obj)};
+        if (get<0>(obj).find(".exe") != string::npos)
+        {
+            if (makefile_copy.find("root") != makefile_copy.end())
             {
-                vertex[j] = {false, 1};
-                tmp_h_eq_1.push_back(temp);
+                get<0>(makefile_copy["root"]).push_back(get<0>(obj));
             }
             else
             {
-                vertex[j] = {false, -1};
+                makefile_copy["root"] = make_tuple(vector<string>{get<0>(obj)}, 0);
             }
         }
     }
 
-    for (auto &i : tmp_h_eq_1)
-    {
-        edgeD temp;
-        temp.fromV = i.toV;
-        temp.toV = "start";
-        temp.weight = 0;
-        graph.push_back(temp);
-    }
-    vertex["start"] = {false, 0};
+    computeHeight(makefile_copy, "root", 0);
 
-    for (const auto &e : graph)
-    {
-        cout << "Edge from " << e.fromV << " to " << e.toV << " with weight " << e.weight << endl;
-    }
-
-    function<int(const string &)> calculate_height = [&](const string &v) -> int
-    {
-        if (vertex[v].height != -1)
-            return vertex[v].height;
-
-        int max_height = 0;
-        for (const auto &e : graph)
-        {
-            if (e.toV == v)
-            {
-                max_height = max(max_height, calculate_height(e.fromV) + 1);
-            }
-        }
-        vertex[v].height = max_height;
-        return max_height;
-    };
-
-    vector<int> pre_ans;
-    pre_ans.push_back(0);
-    for (auto &v : vertex)
-    {
-        if (v.second.height == -1)
-        {
-            int c = calculate_height(v.first);
-            /*if (c>pre_ans.size()){
-
-            }*/
-        }
-    }
-
-for (const auto &v : vertex)
-{
-    cout << "Vertex: " << v.first << ", IsLeaf: " << v.second.isLeaf << ", Height: " << v.second.height << endl;
-}
+    string visual = base64_encode(Generate_Graphviz(makefile_copy));
+    WLOG(visual);
 
     return 0;
 }
